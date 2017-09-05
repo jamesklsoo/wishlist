9ebe0c7eceee5aa08d932c2baeeb17577442d024
James Soo <jamesklsoo@gmail.com>
2017-09-05 16:45:46 +0800

    update user controller and wishes and user migration

--- db/migrate/20170905074417_create_wishes.rb
+++ db/migrate/20170905074417_create_wishes.rb
@@ -1,7 +1,9 @@
 class CreateWishes < ActiveRecord::Migration[5.1]
   def change
     create_table :wishes do |t|
-
+      t.string :name
+      t.text :description
+      t.string :picture
       t.timestamps
     end
   end
