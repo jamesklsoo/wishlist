f911590595b9010d728f9d0e5697fd8a2d5ba662
James Soo <jamesklsoo@gmail.com>
2017-09-06 11:06:09 +0800

    update wishes controller

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -7,7 +7,8 @@ class WishesController < ApplicationController
   end
 
   def show
-  end
+    @wishes =
+      end
 
   def new
     @wish = Wish.new
@@ -18,10 +19,46 @@ class WishesController < ApplicationController
 
   def create
     @wish = Wish.new(wish_params)
+
+    respond_to do |format|
+      if @wish.save
+        format.html { redirect_to @wish, notice: 'Wish was successfully created.'}
+        format.json { render :show, status: :created, location: @wish}
+      else
+        format.html { render :new }
+        format.json { render json: @wish.errors, status: :unprocessable_entity}
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @wish.update(wish_params)
+        format.html { redirect_to @wish, notice: 'Wish was successfully updated.'}
+        format.json { render :show, status: :ok, location: @wish }
+      else
+        format.html { render :edit }
+        format.json { render json: @wish.errors, status: :unprocessable_entity}
+      end
+    end
   end
 
+  def destroy
+    @wish.destroy
+    respond_to do |format|
+      format.html { redirect_to wishes_url, notice: 'Wish was successfully destroyed.'}
+      format.json { head :no_content}
+    end
+  end
+
+  private
+
+  def set_wish
+    @wish = Wish.find(params[:id])
+  end
+
+  def wish_params
+    params.require(:wish).permit(:name, :description, :picture)
+  end
 
 end
