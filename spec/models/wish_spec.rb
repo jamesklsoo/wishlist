bc709c2364578ba43bc36517205e54607aac9336
James Soo <jamesklsoo@gmail.com>
2017-09-08 12:42:26 +0800

    rspec test

---
+++ spec/models/wish_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Wish, type: :model do
+  context "validations" do
+    it "should have description" do
+      should have_db_column(:description).of_type(:string)
+    end
+
+    describe "does not validates presence of attributes" do
+      it { should_not validate_presence_of(:description)}
+    end
+  end
+
+  context "associations" do
+    it {is_expected.to belongs_to(:user)}
+  end
+end
